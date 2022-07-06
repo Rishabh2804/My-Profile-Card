c04bcf07407b4f27b2de05b29a86767a91446b57
Rishabh2804 <rishabhgupta2804@gmail.com>
2022-07-06 15:21:14 +0530

    Font changes and minor fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: avoid_unnecessary_containers
 
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_boxicons/flutter_boxicons.dart';
 import 'package:toast/toast.dart';
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     ToastContext().init(context);
 
-    var phoneNumber = '+1 234 567 890';
+    var phoneNumber = '+1 234 567 8901';
     var email = 'rishabhgupta2804@gmail.com';
     var github = 'Rishabh2804';
     var linkedIn = 'rishabh-gupta-2804';
@@ -77,133 +77,134 @@ class MyApp extends StatelessWidget {
                 SizedBox(
                   width: 400,
                   child: Card(
-                    margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
                     shadowColor: Colors.teal,
                     color: const Color.fromARGB(255, 202, 249, 249),
-                      child: ListTile(
-                        onTap: () => launchURL('tel:$phoneNumber'),
-                        leading: const Icon(
-                          Icons.phone,
-                          color: Color.fromARGB(255, 74, 136, 152),
-                          size: 25,
-                        ),
-                        trailing: Text(
-                          phoneNumber,
-                          style: GoogleFonts.pacifico(
-                            fontSize: 20,
-                            color: const Color.fromARGB(255, 74, 136, 152),
-                            // fontWeight: FontWeight.w600,
-                          ),
+                    child: ListTile(
+                      onTap: () => launchURL('tel:$phoneNumber'),
+                      leading: const Icon(
+                        Icons.phone,
+                        color: Color.fromARGB(255, 74, 136, 152),
+                        size: 25,
+                      ),
+                      trailing: Text(
+                        phoneNumber,
+                        style: GoogleFonts.pacifico(
+                          fontSize: 20,
+                          color: const Color.fromARGB(255, 74, 136, 152),
+                          // fontWeight: FontWeight.w600,
                         ),
                       ),
                     ),
                   ),
-                
+                ),
                 SizedBox(
                   // key: key,
                   width: 400,
                   child: Card(
-                    margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
                     shadowColor: Colors.teal,
                     color: const Color.fromARGB(255, 202, 249, 249),
-                      child: ListTile(
-                        onTap: () => launchURL('mailto:$email'),
-                        leading: const Icon(
-                          Icons.mail,
-                          color: Color.fromARGB(255, 152, 74, 110),
-                          size: 25,
-                        ),
-                        // hoverColor: const Color.fromARGB(255, 182, 228, 235),
-                        trailing: Text(
-                          email,
-                          style: GoogleFonts.niconne(
-                            fontSize: 25,
-                            color: const Color.fromARGB(255, 152, 74, 110),
-                            fontWeight: FontWeight.w600,
-                          ),
+                    child: ListTile(
+                      onTap: () => launchURL('mailto:$email'),
+                      leading: const Icon(
+                        Icons.mail,
+                        color: Color.fromARGB(255, 152, 74, 110),
+                        size: 25,
+                      ),
+                      // hoverColor: const Color.fromARGB(255, 182, 228, 235),
+                      trailing: Text(
+                        email,
+                        style: GoogleFonts.niconne(
+                          fontSize: 21,
+                          color: const Color.fromARGB(255, 152, 74, 110),
+                          // fontWeight: FontWeight.w600,
                         ),
                       ),
                     ),
                   ),
-                
+                ),
                 SizedBox(
                   width: 400,
                   child: Card(
-                    margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
                     shadowColor: Colors.teal,
                     color: const Color.fromARGB(255, 202, 249, 249),
-                      child: ListTile(
-                        onTap: () =>
-                            launchURL('https://www.linkedin.com/in/$linkedIn'),
-                        leading: const Icon(
-                          Boxicons.bxl_linkedin_square,
-                          color: Color.fromARGB(255, 0, 0, 255),
-                          size: 25,
-                        ),
-                        trailing: Text(
-                          linkedIn,
-                          style: GoogleFonts.niconne(
-                            fontSize: 28,
-                            color: const Color.fromARGB(255, 0, 0, 255),
-                          ),
+                    child: ListTile(
+                      onTap: () =>
+                          launchURL('https://www.linkedin.com/in/$linkedIn'),
+                      leading: const Icon(
+                        Boxicons.bxl_linkedin_square,
+                        color: Color.fromARGB(255, 0, 0, 255),
+                        size: 25,
+                      ),
+                      trailing: Text(
+                        linkedIn,
+                        style: GoogleFonts.libreFranklin(
+                          fontSize: 20,
+                          color: const Color.fromARGB(255, 0, 0, 255),
                         ),
                       ),
                     ),
                   ),
-                
-                SizedBox(                  
-                  // key: key,
-                  width: 400,
-                  child: Card(
-                    margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
-                    shadowColor: Colors.teal,
-                    color: const Color.fromARGB(255, 202, 249, 249),
-                      child: ListTile(
-                        onTap: () => launchURL('https://github.com/$github'),
-                        leading: const Icon(
-                          Boxicons.bxl_github,
-                          color: Color.fromARGB(255, 0, 0, 0),
-                          size: 25,
-                        ),
-                        trailing: Text(
-                          github,
-                          style: GoogleFonts.originalSurfer(
-                            fontSize: 20,
-                            color: const Color.fromARGB(255, 0, 0, 0),
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),                
+                ),
                 SizedBox(
                   // key: key,
                   width: 400,
                   child: Card(
-                    margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
                     shadowColor: Colors.teal,
                     color: const Color.fromARGB(255, 202, 249, 249),
-                      child: ListTile(
-                        onTap: () => launchURL(
-                            'https://stackoverflow.com/users/17798976/$stackOverflow'),
-                        leading: const Icon(
-                          Boxicons.bxl_stack_overflow,
+                    child: ListTile(
+                      onTap: () => launchURL('https://github.com/$github'),
+                      leading: const Icon(
+                        Boxicons.bxl_github,
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        size: 25,
+                      ),
+                      trailing: Text(
+                        github,
+                        style: GoogleFonts.originalSurfer(
+                          fontSize: 20,
+                          color: const Color.fromARGB(255, 0, 0, 0),
+                          // fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  // key: key,
+                  width: 400,
+                  child: Card(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                    shadowColor: Colors.teal,
+                    color: const Color.fromARGB(255, 202, 249, 249),
+                    child: ListTile(
+                      onTap: () => launchURL(
+                          'https://stackoverflow.com/users/17798976/$stackOverflow?tab=profile'),
+                      leading: const Icon(
+                        Boxicons.bxl_stack_overflow,
+                        color: Color.fromARGB(255, 218, 123, 39),
+                        size: 25,
+                      ),
+                      // hoverColor: const Color.fromARGB(255, 182, 228, 235),
+                      trailing: Text(
+                        stackOverflow,
+                        style: GoogleFonts.comingSoon(
+                          fontSize: 20,
                           color: Color.fromARGB(255, 218, 123, 39),
-                          size: 25,
-                        ),
-                        // hoverColor: const Color.fromARGB(255, 182, 228, 235),
-                        trailing: Text(
-                          stackOverflow,
-                          style: GoogleFonts.niconne(
-                            fontSize: 25,
-                            color: Color.fromARGB(255, 218, 123, 39),
-                            // fontWeight: FontWeight.w600,
-                          ),
+                          // fontWeight: FontWeight.w600,
                         ),
                       ),
                     ),
                   ),
-                
+                ),
               ],
             ),
           ),
